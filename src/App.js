d4796645c1053edf3ac596b6265331dc2c3c6345
Dung Phan <fshu.pbdn@gmail.com>
2023-04-20 22:15:05 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
 import "./App.css";
-import AddTaskForm from "./components/AddTaskForm";
+// import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: "task_1", title: "Learn JS", status: 0 },
-    { id: "task_2", title: "Code code code", status: 0 },
-  ]);
-  const [showIncomplete, setShowIncomplete] = useState(false);
-  const [newTask, setNewTask] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (newTask) {
-      const task = {
-        id: Date.now(),
-        title: newTask,
-        status: 0,
-      };
-      setTasks([...tasks, task]);
-      setNewTask("");
-    }
-  };
+  // const [tasks, setTasks] = useState([
+  //   { id: "task_1", title: "Learn JS", status: 0 },
+  //   { id: "task_2", title: "Code code code", status: 0 },
+  // ]);
+  // const [showIncomplete, setShowIncomplete] = useState(false);
+  // const [newTask, setNewTask] = useState("");
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (newTask) {
+  //     const task = {
+  //       id: Date.now(),
+  //       title: newTask,
+  //       status: 0,
+  //     };
+  // setTasks([...tasks, task]);
+  // setNewTask("");
+  //   }
+  // };
 
-  const handleInputChange = (e) => {
-    setNewTask(e.target.value);
-  };
+  // const handleInputChange = (e) => {
+  //   setNewTask(e.target.value);
+  // };
 
-  const setTaskStatus = (taskId, status) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === taskId) {
-          return { ...task, status: status ? 1 : 0 };
-        }
-        return task;
-      })
-    );
-  };
+  // const setTaskStatus = (taskId, status) => {
+  //   setTasks(
+  //     tasks.map((task) => {
+  //       if (task.id === taskId) {
+  //         return { ...task, status: status ? 1 : 0 };
+  //       }
+  //       return task;
+  //     })
+  //   );
+  // };
 
-  const removeTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
+  // const removeTask = (taskId) => {
+  //   setTasks(tasks.filter((task) => task.id !== taskId));
+  // };
 
   return (
     <div className="container">
       <Header title="Todo List" subTitle="Get done on time" />
       {/* <Header title="OK" subTitle="" /> */}
-      <TaskList
+      {/* <TaskList
         tasks={tasks}
         showIncomplete={showIncomplete}
         setTaskStatus={setTaskStatus}
         removeTask={removeTask}
         setShowIncomplete={setShowIncomplete}
-      />
+      /> */}
+      <TaskList />
 
-      <AddTaskForm
+      {/* <AddTaskForm
         handleSubmit={handleSubmit}
         newTask={newTask}
         handleInputChange={handleInputChange}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskItem from "./TaskItem";
+import AddTaskForm from "./AddTaskForm";
 
-function TaskList({
-  tasks,
-  showIncomplete,
-  setTaskStatus,
-  removeTask,
-  setShowIncomplete,
-}) {
+// function TaskList({
+//   tasks,
+//   showIncomplete,
+//   setTaskStatus,
+//   removeTask,
+//   setShowIncomplete,
+// }) {
+function TaskList() {
+  const [tasks, setTasks] = useState([
+    { id: "task_1", title: "Learn JS", status: 0 },
+    { id: "task_2", title: "Code code code", status: 0 },
+  ]);
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const setTaskStatus = (taskId, status) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status: status ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
   return (
     <>
       <ul className="task-list">
@@ -34,6 +73,11 @@ function TaskList({
           onChange={(e) => setShowIncomplete(e.target.checked)}
         />
       </div>
+      <AddTaskForm
+        handleSubmit={handleSubmit}
+        newTask={newTask}
+        handleInputChange={handleInputChange}
+      />
     </>
   );
 }
